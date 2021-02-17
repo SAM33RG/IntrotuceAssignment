6d42cc586843f6b7047b294346421bb59ed3a078
sam33r <sameergangar1@gmail.com>
2021-02-17 22:42:25 +0530

    tested firestore

--- app/src/main/java/tech/sam33rg/introtuceassignment/activitiy/MainActivity.java
+++ app/src/main/java/tech/sam33rg/introtuceassignment/activitiy/MainActivity.java
@@ -1,16 +1,91 @@
 package tech.sam33rg.introtuceassignment.activitiy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import tech.sam33rg.introtuceassignment.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "************";
+    Integer number = 1234567890;
+    Integer old = 1234567891;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", "sameer");
+        user.put("lastName", "gangar");
+        user.put("dateOfBirth", "11/08/1999");
+        user.put("gender", "male");
+        user.put("country", "IN");
+        user.put("state", "delhi");
+        user.put("homeTown", "delhi");
+        user.put("phoneNumber", old);
+        user.put("telephoneNumber", 123123214);
+        db.collection("users")
+                .whereEqualTo("phoneNumber", number)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, ""+task.getResult().size());
+
+                            if(task.getResult().size()==0){
+                                Log.d(TAG, "number not present");
+
+                            }
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+
+
+
+// Add a new document with a generated ID
+       /* db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });*/
+
+
+
+
     }
 }
